19c720f51b414f2cabe436a3a47763721cd5ac93
Lee Gillentine <lee@fishburd.com>
2013-03-02 16:08:05 -0500

    POC, sort of

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+/*jslint white: true, undef: true */
 (function(){
-  alert('hello!');
-})
+
+  //the minimum version of jQuery we want
+  var v = "1.9.1";
+
+  //check prior inclusion and version
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false,
+        script = document.createElement("script");
+    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        initMyBookmarklet();
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
+
+function initMyBookmarklet() {
+  alert('hello');
+}
+
+})();
